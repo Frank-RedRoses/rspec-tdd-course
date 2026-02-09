61791af07de8cd8120d775cb87312b098a418cbe
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2026-02-09 15:46:09 +0100

    feat: add custom error message for failing test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -31,6 +31,8 @@ RSpec.describe Card do
   end
 
   it 'has a suit' do
-    expect(card.suit).to eq('Spades')
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} "\
+    "but I got #{card.suit} instead!"
   end
 end
